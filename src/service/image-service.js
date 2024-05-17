9fda07474863c3480b79a8b95ef6cdf7e4a064c6
samuelandrey <samuelandrey2021@gmail.com>
2024-05-18 02:57:09 +0700

    edit: refactor code

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,9 +1,27 @@
-const T = require("tesseract.js");
+const Tesseract = require("tesseract.js");
 
+/**
+ * =============================================================
+ * Image text reader.
+ * =============================================================
+ * Read text from image.
+ * GET Image from url.
+ */
 const reader = async (img) => {
-  const out = await T.recognize(img, 'ind', { logger: e => console.log(e) });
+  const out = await Tesseract.recognize(img, 'ind', {
+    logger: e => console.log(e)
+  });
 
   const lines = out.data.text.split('\n');
+  return parseResult(lines)
+}
+
+/**
+ * Parse result text, custom template here!
+ * @param lines
+ * @returns {*}
+ */
+function parseResult(lines) {
   return lines.map(line => {
     const keyValueArray = line.split(':');
     if (keyValueArray.length === 2) {
--- src/service/pdf-service.js
+++ src/service/pdf-service.js
@@ -1,81 +1,98 @@
 const axios = require("axios");
 const fs = require("fs");
+const pdfParse = require('pdf-parse');
 const Tesseract = require('tesseract.js');
-const {PdfDocument} = require("@ironsoftware/ironpdf");
-const T = require("tesseract.js");
-
+const { PdfDocument } = require("@ironsoftware/ironpdf");
 
 /**
+ * =============================================================
  * PDF text file reader.
+ * =============================================================
  * Can't read image on PDF file.
+ * GET PDF from cloud.
  */
 const PDFReader = async (pdfUrl) => {
   const tempFilePath = './temp.pdf';
 
-  const response = await axios({
-    url: pdfUrl,
-    method: 'GET',
-    responseType: 'stream',
-  });
-
-  const writer = fs.createWriteStream(tempFilePath);
-  response.data.pipe(writer);
-
-  await new Promise((resolve, reject) => {
-    writer.on('finish', resolve);
-    writer.on('error', reject);
-  });
-
+  await getOnlinePDF(tempFilePath, pdfUrl);
   const pdfData = await pdfParse(tempFilePath);
   const lines = pdfData.text.split('\n');
 
-  const result = lines.map(line => {
-    const keyValueArray = line.split(':');
-    if (keyValueArray.length === 2) {
-      const [key, value] = keyValueArray;
-      return { [key.trim()]: value.trim() };
-    } else {
-      return null;
-    }
-  }).filter(item => item !== null);
-
-  fs.unlink(tempFilePath, (err) => {
-    if (err) console.error("Gagal menghapus file sementara:", err);
-  });
+  const result = parseResult(lines);
+  await unlinkFile(tempFilePath)
 
   return result;
 };
 
 
 /**
+ * =============================================================
  * PDF image reader.
+ * =============================================================
  * Can read image on PDF file.
+ * GET PDF from cloud.
  */
 const PDFImageReader = async (pdfUrl) => {
   const tempFilePath = './temp.pdf';
+  const img = "./sample.png";
 
+  await getOnlinePDF(tempFilePath, pdfUrl);
+  await PdfDocument.fromFile(tempFilePath).then((resolve) => {
+    resolve.rasterizeToImageFiles(img);
+    return resolve;
+  });
+
+  const out = await Tesseract.recognize(img, 'ind', {
+    logger: e => console.log(e)
+  });
+
+  const lines = out.data.text.split('\n');
+  const result = parseResult(lines);
+  await unlinkFile(tempFilePath);
+  await unlinkFile(img);
+
+  return result;
+}
+
+/**
+ * Get online pdf to local.
+ * @param path
+ * @param url
+ * @returns {Promise<void>}
+ */
+async function getOnlinePDF(path, url) {
   const response = await axios({
-    url: pdfUrl,
+    url: url,
     method: 'GET',
     responseType: 'stream',
   });
 
-  const writer = fs.createWriteStream(tempFilePath);
+  const writer = fs.createWriteStream(path);
   response.data.pipe(writer);
 
   await new Promise((resolve, reject) => {
     writer.on('finish', resolve);
     writer.on('error', reject);
   });
-  const img = "./sample.png";
-  await PdfDocument.fromFile(tempFilePath).then((resolve) => {
-    resolve.rasterizeToImageFiles(img);
-    return resolve;
+}
+
+/**
+ * Unlink file from storage.
+ * @param path
+ * @returns {Promise<void>}
+ */
+async function unlinkFile(path) {
+  fs.unlink(path, (err) => {
+    if (err) console.error("Gagal menghapus file sementara:", err);
   });
+}
 
-  const out = await Tesseract.recognize(img, 'ind', { logger: e => console.log(e) });
-
-  const lines = out.data.text.split('\n');
+/**
+ * Parse text result, custom template here!
+ * @param lines
+ * @returns {*}
+ */
+function parseResult(lines) {
   return lines.map(line => {
     const keyValueArray = line.split(':');
     if (keyValueArray.length === 2) {
@@ -86,7 +103,6 @@ const PDFImageReader = async (pdfUrl) => {
     }
   }).filter(item => item !== null);
 }
-
 
 module.exports = {
   PDFReader,
